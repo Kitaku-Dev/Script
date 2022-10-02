8e2c5fc0a490fb7e5f95207a6f5ff787e182bf6a
Kitaku-Dev <110123325+Kitaku-Dev@users.noreply.github.com>
2022-10-02 22:14:01 +0700

    Create BypassThunder.lua

---
+++ BypassThunder.lua
@@ -0,0 +1,8 @@
+local placeId = game.PlaceId
+if placeId == 2753915549 then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/Pepekx/QwontolFY/main/Games/BF-PC"))()
+elseif placeId == 4442272183 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pepekx/QwontolFY/main/Games/BF-PC"))()
+elseif placeId == 7449423635 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pepekx/QwontolFY/main/Games/BF-PC"))()
+end
